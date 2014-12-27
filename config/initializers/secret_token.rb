7e98b009dc527faa355e935ea45f14314c271649
Carl Tanner <carl@wdwhub.net>
2014-12-26 17:24:49 -0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parcgatewaycondo::Application.config.secret_key_base = 'e1e2191bd879a70ef0f1d22430eafee873e4e0157e73c57f72ee37ec9889caf34213e19667375d16cf4b131c00a8b3ecf6d25b912758cf52dcfa7f2da10c8f83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Parcgatewaycondo::Application.config.session_store :cookie_store, key: '_parcgatewaycondo_session'
